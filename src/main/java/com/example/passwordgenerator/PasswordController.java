ab760559c8eecc77a779fd95ab652eb06120a697
Katyanne Roberta R. Nunes <betanunes8@gmail.com>
2024-10-29 13:11:01 -0400

    atualizando regra no backend

--- src/main/java/com/example/passwordgenerator/PasswordController.java
+++ src/main/java/com/example/passwordgenerator/PasswordController.java
@@ -1,18 +1,51 @@
 package com.example.passwordgenerator;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api/password")
 public class PasswordController {
 
     @PostMapping("/generate")
-    public String generatePassword(@RequestParam int length, @RequestParam boolean useUpperCase,
-                                   @RequestParam boolean useNumbers, @RequestParam boolean useSpecialChars) {
-        PasswordGenerator generator = new PasswordGenerator(length, useUpperCase, useNumbers, useSpecialChars);
-        return generator.generatePassword();
+    public String generatePassword(@RequestBody PasswordRequest request) {
+        int length = request.getLength();
+
+        // Verifica o numero de caracteres menor que 4
+        if (length < 4) {
+            return "Erro: O comprimento da senha deve ser de no mínimo 4 caracteres.";
+        }
+
+        //Conjunto de caracteres
+        String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numbers = "0123456789";
+        String specialChars = "!@#$%^&*()-_+=<>?";
+
+        StringBuilder characterSet = new StringBuilder();
+
+        // Add os conjuntos de caracteres com base nas escolhas do usuario
+        if (request.isUseUpperCase()) {
+            characterSet.append(upperCaseLetters);
+        }
+        if (request.isUseNumbers()) {
+            characterSet.append(numbers);
+        }
+        if (request.isUseSpecialChars()) {
+            characterSet.append(specialChars);
+        }
+        // Casa nenhum conjunto de caracteres seja escolhido, incluiletras minusculas por padrao
+        if (characterSet.length() == 0) {
+            characterSet.append("abcdefghijklmnopqrstuvwxyz");
+        }
+        // Gera a senha
+        StringBuilder passaword = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characterSet.length());
+            passaword.append(characterSet.charAt(index));
+        }
+        return  passaword.toString();
     }
 }
