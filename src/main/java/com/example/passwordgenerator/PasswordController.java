6c42b0f91bd447756d5250755a943e263ed6edb4
Katyanne Roberta R. Nunes <betanunes8@gmail.com>
2024-10-29 15:11:42 -0400

    correcoes de back

--- src/main/java/com/example/passwordgenerator/PasswordController.java
+++ src/main/java/com/example/passwordgenerator/PasswordController.java
@@ -1,15 +1,20 @@
 package com.example.passwordgenerator;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/password")
 public class PasswordController {
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("/generate")
-    public String generatePassword(@RequestParam int length, @RequestParam boolean useUpperCase,
-                                   @RequestParam boolean useNumbers, @RequestParam boolean useSpecialChars) {
-        PasswordGenerator generator = new PasswordGenerator(length, useUpperCase, useNumbers, useSpecialChars);
+    public String generatePassword(@RequestBody Map<String, Object> requestData) { 
+        int length = (int) requestData.get("length"); 
+        boolean useUpperCase = (boolean) requestData.get("useUpperCase"); 
+        boolean useNumbers = (boolean) requestData.get("useNumbers"); 
+        boolean useSpecialChars = (boolean) requestData.get("useSpecialChars"); 
+        PasswordGenerator generator = new PasswordGenerator(length, useUpperCase, useNumbers, useSpecialChars); 
         return generator.generatePassword();
     }
 }
